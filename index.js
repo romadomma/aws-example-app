fd6c57dfea23297fe7beec6100cd5546689b7c64
Roman Kadykov <rkadykov@plesk.com>
2020-10-09 11:10:20 +0700

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const app = require('express')();
+const bodyParser = require('body-parser');
+let buff = [];
+
+app.use(bodyParser.json({ type: 'application/json' }));
+
+app.get('/health', (req, res) => {
+    res.send('OK');
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    if (req.body.items && Array.isArray(req.body.items))
+    {
+        buff = [...buff, ...req.body.items];
+        res.send('items saved');
+    } else {
+        res.send('items is empty or is not array');
+    }
+});
+
+app.get('/', (req, res) => {
+    res.json(buff).send();
+});
+
+setInterval(() => { buff = []; }, 60000);
+
+app.listen(80);
+
+console.log('Server started on 80 port 🤪');
